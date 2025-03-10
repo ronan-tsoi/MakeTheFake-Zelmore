7b01204cc8f4c85effef46a4dc90fe6f442feae2
ronan-tsoi <156042670+ronan-tsoi@users.noreply.github.com>
2025-03-09 23:51:58 -0700

    win/game over states, health visualizer, some SFX

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -1,12 +1,1 @@
-class Player extends Phaser.GameObjects.Sprite {
-    constructor(scene) {
-        super(scene, 128, height-38, 'player')
-
-        this.parentScene = scene
-
-        this.parentScene.add.existing(this).setOrigin(0,1).setScale(4)
-        this.parentScene.physics.add.existing(this).setScale(2)
-
-        this.body.setCollideWorldBounds(true)
-    }
-}
+//temporary
